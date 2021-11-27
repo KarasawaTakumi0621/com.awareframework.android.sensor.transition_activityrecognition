5a61e0adbdf3a45d5e6e96c152fbcb4fd22ac307
KarasawaTakumi0621 <tappun0621@gmail.com>
2021-11-27 16:46:22 +0900

    first commit

---
+++ app/src/main/java/com/tappun/android/sensor/transition_activityrecognition_exampleapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.tappun.android.sensor.transition_activityrecognition_exampleapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
